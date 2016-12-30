a0ede272c527756ba8f37bf3e3273c707fe181cf
Yukihide YASUKAWA <yasukawa.yukihide@ntt-it.co.jp>
2016-12-31 01:16:34 +0900

    .gvimrcのリンクを作るように

--- create_dotfile_link.bat
+++ create_dotfile_link.bat
@@ -1,6 +1,7 @@
 @echo off
 
 mklink %USERPROFILE%\.vimrc %USERPROFILE%\dotfiles\.vimrc
+mklink %USERPROFILE%\.gvimrc %USERPROFILE%\dotfiles\.gvimrc
 mklink %USERPROFILE%\.vim %USERPROFILE%\dotfiles\.vim
 
 
