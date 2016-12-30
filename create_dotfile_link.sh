5fdb45a9be6f0b87e37f7163e8740b3917b4b860
Yukihide YASUKAWA <yasukawa.yukihide@ntt-it.co.jp>
2016-12-31 01:20:59 +0900

    .gvimrcのリンクを作るように for Mac,linux

--- create_dotfile_link.sh
+++ create_dotfile_link.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/.vim ~/.vim
 #ln -sf ~/dotfiles/colors ~/.vim
 #ln -sf ~/dotfiles/ftdetect ~/.vim
